41f2436c2b720d5deb5df837c4e02eebe48b20fb
VIJET BADIGANNAVAR <bvijet@gmail.com>
2016-12-05 00:23:18 -0500

    Reflected the indexes of the original fields

--- eBirdProject/src/main/scala/PreProcess.scala
+++ eBirdProject/src/main/scala/PreProcess.scala
@@ -11,20 +11,20 @@ object PreProcess {
 
     val preprocessedData = rawData.map(data => {
       val values = data.split(",")
-      (values(0), //LocationId - no change
-       values(1), //Month - no change
-       values(2), //day - no change
-       values(3), //time - no change
-       values(4), //count_type - no change
-       if(values(6).charAt(0)=='?') 1070.58824764576 else values(6),  // POP
-       if(values(7).charAt(0)=='?') 492.831733564141 else values(7),  // Housing Density
-       if(values(8).charAt(0)=='?') 0.13829529929963 else values(8),  // Housing Vacent
-       if(values(9).charAt(0)=='?') 5 else values(9),                 // Avg Temp
-       if(values(10).charAt(0)=='?') 6 else values(10),               // Min Temp
-       if(values(11).charAt(0)=='?') 6 else values(11),              // Max  Temp
-       if(values(12).charAt(0)=='?') 6 else values(12),              // Precipitation
-       values(13), // CAUS_SNOW - no change
-       if(values(5).charAt(0)!='0') 1 else 0 // target value
+      (values(1), //LocationId - no change
+       values(5), //Month - no change
+       values(6), //day - no change
+       values(7), //time - no change
+       values(11), //count_type - no change
+       if(values(955).charAt(0)=='?') 1070.58824764576 else values(955),  // POP
+       if(values(956).charAt(0)=='?') 492.831733564141 else values(956),  // Housing Density
+       if(values(957).charAt(0)=='?') 0.13829529929963 else values(957),  // Housing Vacent
+       if(values(963).charAt(0)=='?') 5 else values(963),                 // Avg Temp
+       if(values(964).charAt(0)=='?') 6 else values(964),               // Min Temp
+       if(values(965).charAt(0)=='?') 6 else values(965),              // Max  Temp
+       if(values(966).charAt(0)=='?') 6 else values(966),              // Precipitation
+       values(967), // CAUS_SNOW - no change
+       if(values(26).charAt(0)!='0') 1 else 0 // target value
       )
     })
 
