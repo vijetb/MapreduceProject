e00bb111ea4028aa67b0e2acf78a885e834bacb9
Samarth Shetty <samarthrshetty@gmail.com>
2016-12-08 20:03:13 -0500

    Code Runs

--- eBirdProject/src/main/scala/PreProcess.scala
+++ eBirdProject/src/main/scala/PreProcess.scala
@@ -1,55 +1,119 @@
 import org.apache.spark.mllib.feature.Normalizer
-import org.apache.spark.mllib.feature.StandardScaler
+import org.apache.spark.ml.feature.StandardScaler
 import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.ml.feature._
+import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.sql.types.{StructField, StructType}
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.sql.functions._
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.{RandomForestClassificationModel, RandomForestClassifier}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.MaxAbsScaler
 
+import org.apache.spark.sql.types.{DataTypes, StructType}
 object PreProcess {
   def main(args: Array[String]){
     val conf = new SparkConf()
       .setAppName("eBird Project")
-      .setMaster("local")
-
+      .setMaster("local[*]")
     val sc = new SparkContext(conf)
-    val rawData = sc.textFile("input/testing_sample.csv")
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+    val rawData = sc.textFile("input/labeledsmall.csv")
 
-    val rawDataWithougHeader =  rawData.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
+    val rawDataWithoutHeader =  rawData.mapPartitionsWithIndex { (idx, iter) => if (idx == 0) iter.drop(1) else iter }
+        val preprocessedData = rawDataWithoutHeader.map(data => {
+          val values = data.split(",")
+          (if(values(26).charAt(0)!='0') 1.0.toString else 0.0.toString, // target value
+           values(2), //Lat - no change //
+           values(3), //Long - no change //
+           values(5), //Month - no change //
+           values(6), //day - no change
+           values(7), //time - no change
+           if(values(955).charAt(0)=='?') null else values(955),  // POP 6
+           if(values(956).charAt(0)=='?') null else values(956),  // Housing Density
+           if(values(957).charAt(0)=='?') null else values(957),  // Housing Vacent
+           if(values(963).charAt(0)=='?') null else values(963),  // Avg Temp
+           if(values(964).charAt(0)=='?') null else values(964),  // Min Temp
+           if(values(965).charAt(0)=='?') null else values(965),  // Max  Temp
+           if(values(966).charAt(0)=='?') null else values(966),  // Precipitation
+           if(values(967).charAt(0)=='?') "0" else values(967)    // CAUS_SNOW - if ? make it 0
+          )})
+
+    val colNames = Array("Label","LAT", "LONG","MONTH","DAY","TIME","POP00_SQMI","HOUSING_DENSITY","HOUSING_PERCENT_VACANT","CAUS_TEMP_AVG","CAUS_TEMP_MIN","CAUS_TEMP_MAX","CAUS_PREC", "CAUS_SNOW")
+
+    val colRequiredToBeImputed = Array("POP00_SQMI","HOUSING_DENSITY","HOUSING_PERCENT_VACANT","CAUS_TEMP_AVG","CAUS_TEMP_MIN","CAUS_TEMP_MAX","CAUS_PREC")
+
+    val categoricalCol = Array("CAUS_TEMP_AVG","CAUS_TEMP_MIN","CAUS_TEMP_MAX","CAUS_PREC")
+
+    val featureList = Array("LAT", "LONG","MONTH","DAY","TIME","POP00_SQMI","HOUSING_DENSITY","HOUSING_PERCENT_VACANT","CAUS_TEMP_AVG","CAUS_TEMP_MIN","CAUS_TEMP_MAX","CAUS_PREC", "CAUS_SNOW")
+
+    import sqlContext.implicits._
+    val df = preprocessedData.toDF(colNames: _*)
 
 
-    val preprocessedData = rawDataWithougHeader.map(data => {
-      val values = data.split(",")
-      (values(1), //LocationId - no change
-       values(5), //Month - no change
-       values(6), //day - no change
-       values(7), //time - no change
-       values(11), //count_type - no change
-       if(values(955).charAt(0)=='?') 1070.58824764576 else values(955),  // POP
-       if(values(956).charAt(0)=='?') 492.831733564141 else values(956),  // Housing Density
-       if(values(957).charAt(0)=='?') 0.13829529929963 else values(957),  // Housing Vacent
-       if(values(963).charAt(0)=='?') 5 else values(963),                 // Avg Temp
-       if(values(964).charAt(0)=='?') 6 else values(964),               // Min Temp
-       if(values(965).charAt(0)=='?') 6 else values(965),              // Max  Temp
-       if(values(966).charAt(0)=='?') 6 else values(966),              // Precipitation
-       if(values(967).charAt(0)=='?') 0 else values(967),              // CAUS_SNOW - if ? make it 0
-       if(values(26).charAt(0)!='0') 1 else 0 // target value
-      )
-    })
+    val imputer = df.select(colRequiredToBeImputed.map(avg(_)):_*).toDF(colRequiredToBeImputed: _*)
+    val imputerMap = imputer.columns.zip(imputer.first().toSeq).
+      map(a =>
+        if (categoricalCol contains  a._1)
+          (a._1 -> a._2.toString.toDouble.ceil.toString)
+        else
+          (a._1 -> a._2)).toMap
+    val imputedDataFrameStringRep = df.na.fill(imputerMap)
+    val completeData= imputedDataFrameStringRep.select(imputedDataFrameStringRep.columns.map
+    (imputedDataFrameStringRep.col(_).cast(DataTypes.DoubleType)) : _*)
+    val assembler = new VectorAssembler()
+      .setInputCols(featureList)
+      .setOutputCol("features")
+    val output = assembler.transform(completeData)
+    val scaler = new StandardScaler()
+      .setInputCol("features")
+      .setOutputCol("scaledFeatures")
+      .setWithStd(true)
+      .setWithMean(false)
+    // Compute summary statistics and generate MaxAbsScalerModel
+    val scalerModel = scaler.fit(output)
+    val scaledData = scalerModel.transform(output)
 
 
-    System.out.println(rawData.count())
-    System.out.println(preprocessedData.count())
-
-    preprocessedData.coalesce(1,true).saveAsTextFile("output")
-//    val normSampleData = sc.textFile("input/norm_test")
-//    val normSampleDataRDD = normSampleData.map(x => {
-//      val values = x.split(",")
-//      Vectors.dense(values(0).toDouble,values(1).toDouble,values(2).toDouble,values(3).toDouble,values(4).toDouble)
-//    })
+    //
+//    output.printSchema
 //
-////    val normalizer1 = new Normalizer()
-////    val data1 = normSampleDataRDD.map(x => (normalizer1.transform(x)))
-//    val scaler = new StandardScaler(withMean = true, withStd = true).fit(normSampleDataRDD)
-//      // Scale features using the scaler model
-//    val scaledFeatures = scaler.transform(normSampleDataRDD)
-//    scaledFeatures.coalesce(1,true).saveAsTextFile("output")
+//
+//    val featureIndexer = new VectorIndexer()
+//      .setInputCol("features")
+//      .setOutputCol("indexedFeatures")
+//      .setMaxCategories(100)
+//
+//    val categoricalFeaturesTransformed = featureIndexer.fit(output).transform(output)
+//
+    val Array(trainingData, testData) = scaledData.randomSplit(Array(0.8, 0.2))
+//
+    // Train a RandomForest model.
+    val rf = new RandomForestClassifier()
+      .setLabelCol("Label")
+      .setFeaturesCol("scaledFeatures")
+      .setNumTrees(100)
+
+    // Chain indexers and forest in a Pipeline.
+    val pipeline = new Pipeline()
+      .setStages(Array(rf))
+
+    // Train model. This also runs the indexers.
+    val model = pipeline.fit(trainingData)
+
+    // Make predictions.
+    val predictions = model.transform(testData)
+    // Select (prediction, true label) and compute test error.
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("Label")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+    val accuracy = evaluator.evaluate(predictions)
+    println("Test Error = " + (1.0 - accuracy))
+
+    val rfModel = model.stages(0).asInstanceOf[RandomForestClassificationModel]
+    println("Learned classification forest model:\n" + rfModel.toDebugString)
   }
 }
